0b35f8d31ef404282f442da780cec5f7fe5b7126
dibikadahal <dibikadahal@gmail.com>
2026-03-10 05:31:23 +0545

    set the players choice to the selected weapon

--- games/rock_paper_scissors/rock_paper_scissors.js
+++ games/rock_paper_scissors/rock_paper_scissors.js
@@ -2,11 +2,30 @@ let container = document.querySelector(".container");
 let weaponBox = container.querySelector(".weapon-box");
 let playerChoicesBox = container.querySelector(".player-choices");
 let weapons = weaponBox.querySelectorAll(".weapons div");
+let player = playerChoicesBox.querySelector(".player-choice img");
+let computer = playerChoicesBox.querySelector(".computer-choice img");
 
 //Add event listeners to each weapon choice
 for (let i = 0; i < weapons.length; i++){
     weapons[i].addEventListener("click", (e)=>{
+
+        //Hide the weapon box and show the player choices
         weaponBox.style.display = "none";
         playerChoicesBox.style.display = "block";
-    })
+
+        // Add a delay before showing the player choices
+        setTimeout(() => {
+            playerChoicesBox.classList.add("active");
+        }, 1000);
+
+        setTimeout(() => {
+            let playerChoices = playerChoicesBox.querySelectorAll("div");
+            for(let i = 0; i< playerChoices.length; i++){
+                playerChoices[i].style.animationPlayState = "paused";
+            } 
+
+            // set the player choice to the selected weapon
+            player.src = e.target.src;
+    }, 3000);
+})
 }
